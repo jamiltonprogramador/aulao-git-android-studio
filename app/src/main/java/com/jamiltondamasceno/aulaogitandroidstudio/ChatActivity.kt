6fdbe3e6bf9a1493bc20ebc80dc246e60e9a3655
Jamilton Damasceno <jamilton.cursos2@gmail.com>
2023-09-20 20:18:17 -0300

    3-chat Adicionado recurso de chat, com envio de mensagens

---
+++ app/src/main/java/com/jamiltondamasceno/aulaogitandroidstudio/ChatActivity.kt
@@ -0,0 +1,16 @@
+package com.jamiltondamasceno.aulaogitandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+
+    fun enviarMensagem(){
+
+    }
+
+}
